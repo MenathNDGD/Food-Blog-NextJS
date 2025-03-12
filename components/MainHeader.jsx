9c6854b3c19feb7769674631c77d52951744c1e1
MenathNDGD <nadungmenath1@gmail.com>
2025-03-12 15:49:07 +0530

    Enhance MainHeader component with navigation links and logo

--- components/MainHeader.jsx
+++ components/MainHeader.jsx
@@ -1,5 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import Logo from "@/assets/logo-full.png";
+
 const MainHeader = () => {
-  return <div>MainHeader</div>;
+  return (
+    <div className="flex items-center justify-between">
+      <Link href={"/"}>
+        <Image src={Logo} alt="Logo" width={200} height={50} className="p-5" />
+      </Link>
+      <nav className="flex flex-col items-center space-x-5 p-5">
+        <ul className="flex items-center space-x-5">
+          <li>
+            <Link href={"/meals"}>Browse Meals</Link>
+          </li>
+          <li>
+            <Link href={"/community"}>Foodies Community</Link>
+          </li>
+        </ul>
+      </nav>
+    </div>
+  );
 };
 
 export default MainHeader;
