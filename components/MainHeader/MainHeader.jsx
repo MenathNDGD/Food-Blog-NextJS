1788682c9c2aba6a9c08be66433eed0efa109c6a
MenathNDGD <nadungmenath1@gmail.com>
2025-03-12 18:37:13 +0530

    Add MainHeader component with navigation and logo

---
+++ components/MainHeader/MainHeader.jsx
@@ -0,0 +1,32 @@
+import Link from "next/link";
+import Image from "next/image";
+
+import classes from "./MainHeader.module.css";
+
+import Logo from "@/assets/logo-full.png";
+import MainHeaderBackground from "./MainHeaderBackground";
+
+const MainHeader = () => {
+  return (
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href={"/"} className={classes.logo}>
+          <Image src={Logo} alt="Logo" width={200} height={50} priority />
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Link href={"/meals"}>Browse Meals</Link>
+            </li>
+            <li>
+              <Link href={"/community"}>Foodies Community</Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+};
+
+export default MainHeader;
