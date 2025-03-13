ecc27d286708dd26a4ea09deee6cc1bef18c6d2a
MenathNDGD <nadungmenath1@gmail.com>
2025-03-13 19:52:17 +0530

    Enhance MealsPage layout with header and MealsGrid component

--- app/meals/page.jsx
+++ app/meals/page.jsx
@@ -1,5 +1,35 @@
+import Link from "next/link";
+
+import classes from "./page.module.css";
+
+import MealsGrid from "@/components/Meals/MealsGrid";
+
 const MealsPage = () => {
-  return <div>MealsPage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Delicious Meals, Created{" "}
+          <span className={classes.highlight}>By You!</span>
+        </h1>
+        <p>
+          Choose your favorite meal and enjoy a delicious lunch or dinner at
+          home.
+        </p>
+        <p>
+          All our meals are cooked with high-quality ingredients, just-in-time
+          and of course by experienced
+          <span className={classes.highlight}> chefs!</span>
+        </p>
+        <p className={classes.cta}>
+          <Link href={"/meals/share"}>Share Your Favorite Recipe!</Link>
+        </p>
+      </header>
+      <main className={classes.main}>
+        <MealsGrid meals={[]} />
+      </main>
+    </>
+  );
 };
 
 export default MealsPage;
