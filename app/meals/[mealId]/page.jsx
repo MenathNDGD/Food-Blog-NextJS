52bafa9b4ebc3673a28ab8560f9521c5fc48e44d
MenathNDGD <nadungmenath1@gmail.com>
2025-03-14 20:47:46 +0530

    Implement meal details page with dynamic content and styling

--- app/meals/[mealId]/page.jsx
+++ app/meals/[mealId]/page.jsx
@@ -1,5 +1,37 @@
-const MealDetailsPage = () => {
-  return <div>MealDetailsPage</div>;
+import Image from "next/image";
+import Link from "next/link";
+
+import classes from "./page.module.css";
+
+import { getMealDetails } from "@/lib/meals";
+
+const MealDetailsPage = ({ params }) => {
+  const meal = getMealDetails(params.mealId);
+
+  return (
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill />
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <Link href={`mailto:${meal.creator_email}`}>{meal.creator}</Link>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p
+          className={classes.instructions}
+          dangerouslySetInnerHTML={{
+            __html: meal.instructions.replace(/\n/g, "<br />"),
+          }}
+        ></p>
+      </main>
+    </>
+  );
 };
 
 export default MealDetailsPage;
