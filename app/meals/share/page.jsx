f47d48a029609d9fb34bfec44a5d6360bd4dc28f
MenathNDGD <nadungmenath1@gmail.com>
2025-03-15 09:51:10 +0530

    Implement Share Meal page with form for user input and styling

--- app/meals/share/page.jsx
+++ app/meals/share/page.jsx
@@ -1,5 +1,53 @@
+import classes from "./page.module.css";
+
 const ShareMealPage = () => {
-  return <div>ShareMealPage</div>;
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share Your{" "}
+          <span className={classes.highlight}>Favorite Meal Recipe</span> With
+          The Foodies Community!
+        </h1>
+        <p>Or Any Other Meal You Feel Needs Sharing!</p>
+      </header>
+      <main className={classes.main}>
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Instructions</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          IMAGE PICKER
+          <p className={classes.actions}>
+            <button type="submit">Share Meal</button>
+          </p>
+        </form>
+      </main>
+    </>
+  );
 };
 
 export default ShareMealPage;
